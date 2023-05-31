e93383c55bfbf4d33f6442c8d67ce22a23c08052
Shakil Ahmed <shakilahmedshuvo612@gmail.com>
2023-06-01 00:20:30 +0600

    add make admin api

--- index.js
+++ index.js
@@ -50,6 +50,19 @@ async function run() {
             res.send(result);
         });
 
+        // 
+        app.patch('/users/admin/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) }
+            const updateDoc = {
+                $set: {
+                    role: 'admin'
+                },
+            };
+            const result = await usersCollection.updateOne(filter, updateDoc);
+            res.send(result);
+        })
+
 
         // menu get api
         app.get('/menu', async (req, res) => {
