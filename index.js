14c81939ccc3b3d74b497e9542f2e8e71f7e4688
Shakil Ahmed <shakilahmedshuvo612@gmail.com>
2023-06-04 22:01:43 +0600

    add verify admin api

--- index.js
+++ index.js
@@ -66,8 +66,24 @@ async function run() {
             res.send({ token });
         })
 
-        // user get api
-        app.get('/users', async (req, res) => {
+        // Warning: use verifyJWT before using verifyAdmin
+        const verifyAdmin = async (req, res, next) => {
+            const email = req.decoded.email;
+            const query = { email: email };
+            const user = await usersCollection.findOne(query);
+            if (user?.role !== 'admin') {
+                return res.status(403).send({ error: true, message: 'Forbidden Access' });
+            }
+            next();
+        }
+
+        /*
+            * 1. Do not show secure links to those who should not see the links
+            * 2. Use jwt token: verifyJWT
+            * 3. use verifyAdmin middleware
+        */
+        //  allUser get api
+        app.get('/users', verifyJWT, verifyAdmin, async (req, res) => {
             const result = await usersCollection.find().toArray();
             res.send(result);
         });
